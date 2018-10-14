3538df66747dec16a7508e90c07b5bddb8114aa0
hchope <43537733+hchope@users.noreply.github.com>
2018-10-15 00:02:49 +0300

    Update stockcontrol.py

--- stockcontrol.py
+++ stockcontrol.py
@@ -4,6 +4,7 @@
 ## Work through the 14 steps of the coursework to
 ## implement and extend the methods and classes.
 
+## GROUP MEMBERS
 
 ## NAME: KATEETE  TWAHA    		STUDENT'S NO.: 1800737883    	REG. NO: 2018/HD05/1958U
 ## NAME: WAKIBI CHRISTOPHER 	STUDENT'S NO.: 1800740236     	REG. NO: 2018/HD05/1983U
